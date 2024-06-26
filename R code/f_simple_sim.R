867e358687f183956eedfc4dbd40c286d528c995
yujinggao24 <40865288+Jane-Gauss@users.noreply.github.com>
2024-06-26 08:56:30 -0400

    Update f_simple_sim.R

--- R code/f_simple_sim.R
+++ R code/f_simple_sim.R
@@ -5,118 +5,201 @@ library(nloptr)
 # The treatment assignment for the RPCT data is completely at random 
 # There is no unmeasured confounders
 
-# Generate outcome Y based on linear regression
-f_Y_gen <- function(x,
-                    beta0,
-                    beta1, 
-                    sdY){
-  mu <- beta0 + beta1 * x 
-  return(rnorm(1, mean = mu, sd = sdY))
-}
 
-# Generate EC data
-f_EC_out <- function(seed_EC = 123,
-                     N_EC = 1000,
-                     muX_EC = 1, 
-                     sdX_EC = 1,
-                     beta0 = 1,
-                     beta1 = 2,
-                     sdY = 1){
-  set.seed(seed_EC)
-  # generate the pre-treatment covariates
-  X_EC <- rnorm(N_EC, mean = muX_EC, sd = sdX_EC)
-  
-  Y_EC <- mapply(f_Y_gen,
-                 x = X_EC, 
-                 beta0, beta1, sdY)
-  return(data.frame(X_EC, Y_EC))
-}
 
-# Conditional variance estimation function for EC data
-var_fun <- function(data){
-  res <- 1
-  return(res)
-}
-
-data_EC <- f_EC_out(N_EC = 1000)
-
-# Estimate variance of IF-based estimator using EC data
-Est_var_simple <- function(
-    N_RCT, # sample size of RCT, need to be decided
-    data_EC, # available EC data
-    pi = 0.5, # given propensity socre
-    var_fun, # given function to estimate variance
-    r = 1, # the ratio of variance in EC and variance in control of RCT
-    gamma1 = 1, # the ratio of two kappa
-    d_X = 1 # the ratio of f(X|R=1) with f(X|R=0)
-){
-  # Step 1: estimate V(Y|X,R=0) using data_EC
-  var_X_EC <- var_fun(data_EC)
+#################################################################################
+####### Generate EC data and define variance function for our simple case #######
+{
+  # Generate outcome Y based on linear regression
+  f_Y_gen <- function(x,
+                      beta0,
+                      beta1, 
+                      sdY){
+    mu <- beta0 + beta1 * x 
+    return(rnorm(1, mean = mu, sd = sdY))
+  }
   
-  # Step 2: estimate V(Y|X,R=1,A=0)
-  var_X_RCT_0 <- r * var_X_EC
-  
-  # Step 3: estimate kappa 0 and kappa 1
-  kappa_0 <- mean(var_X_RCT_0)
-  kappa_1 <- gamma1 * kappa_0
-  
-  # Step 4: estimate V(Y|R=1,A=1), V(Y|R=1, A=0)
-  var_EC <- var(data_EC$Y_EC)
-  var_RCT_0 <- var_EC
-  var_RCT_1 <- var_EC
-  
-  # Step 5: calculate d(X)
+  # Generate EC data
+  f_EC_out <- function(seed_EC = 123,
+                       N_EC = 1000,
+                       muX_EC = 1, 
+                       sdX_EC = 1,
+                       beta0 = 1,
+                       beta1 = 2,
+                       sdY = 1){
+    set.seed(seed_EC)
+    # generate the pre-treatment covariates
+    X_EC <- rnorm(N_EC, mean = muX_EC, sd = sdX_EC)
     
-  # Step 6: plug in to get nu^2
-  N <- N_EC + N_RCT
-  r_R <- N_RCT / N_EC
-  term1 <- 1 / N_RCT / pi * mean(var_X_EC)
-  term2 <- 1 / N_RCT * (var_EC -  mean(var_X_RCT_0))
-  term3 <- 1 / N_RCT * mean(r * (1 - pi)/(1 - pi + r/d_X/r_R)^2 * var_X_EC)
-  term4 <- 1 / N_RCT * (var_EC - mean(var_X_RCT_0)) 
-  term5 <- 1 / N_RCT * mean(r^2/r_R/(1 - pi + r/d_X/r_D)^2 * var_X_EC)
-  nu2 <- N * (term1 + term2 + term3 + term4 + term5)
- 
-  return(nu2) 
+    Y_EC <- mapply(f_Y_gen,
+                   x = X_EC, 
+                   beta0, beta1, sdY)
+    return(data.frame(X_EC, Y_EC))
+  }
+  
+  # Conditional variance estimation function for EC data
+  var_fun <- function(data){
+    res <- 1
+    return(res)
+  }
+  
+  data_EC <- f_EC_out(N_EC = 1000)
+  
+  # Estimate variance of IF-based estimator using EC data
+  Est_var_simple <- function(
+    data_EC, # available EC data
+    N_RCT, # sample size of RCT, need to be decided
+    r = 1, # the ratio of variance in EC and variance in control of RCT
+    d_X = 1, # the ratio of f(X|R=1) with f(X|R=0)
+    pi = 0.5, # given propensity score
+    gamma1 = 1 # the ratio of two kappa
+    # var_fun = var_fun # given function to estimate variance
+  ){
+    
+    # Step 0: given propensity score function - skip, let's use 1/2
+    
+    # Step 1: estimate V(Y|X,R=0) using data_EC
+    # var_X_EC <- var_fun(data_EC)
+    var_X_EC <- 1
+    
+    # Step 2: estimate V(Y|X,R=1,A=0)
+    var_X_RCT_0 <- r * var_X_EC
+    
+    # Step 3: estimate kappa 0 and kappa 1
+    kappa_0 <- mean(var_X_RCT_0)
+    kappa_1 <- gamma1 * kappa_0
+    
+    # Step 4: estimate V(Y|R=1,A=1), V(Y|R=1, A=0) 
+    #  Step 4 relies on the d(X) function 
+    var_EC <- var(data_EC$Y_EC)
+    var_RCT_0 <- var_EC
+    var_RCT_1 <- var_EC
+    
+    # Step 5: calculate d(X) - skip, let's use the true value 
+    
+    # Step 6: plug in to get nu^2
+    N_EC <- nrow(data_EC)
+    N <- N_EC + N_RCT
+    r_R <- N_RCT / N_EC
+    term1 <- 1 / N_RCT / pi * mean(var_X_RCT_0)
+    term2 <- 1 / N_RCT * (var_EC -  mean(var_X_RCT_0))
+    term3 <- 1 / N_RCT * mean(r * (1 - pi)/(1 - pi + r / d_X / r_R)^2 * var_X_EC)
+    term4 <- 1 / N_RCT * (var_EC - mean(var_X_RCT_0)) 
+    term5 <- 1 / N_RCT * mean(r^2/r_R/(1 - pi + r / d_X / r_R)^2 * var_X_EC)
+    nu2 <- N * (term1 + term2 + term3 + term4 + term5)
+    
+    return(nu2) 
+  } 
+  
+  Est_var_simple(data_EC, N_RCT = 246)
+  
 }
 
 
+#######################################################################
+####### Use Nonlinear optimization function for our simple case #######
+{
+  
+  # This is the main optimization formula
+  eval_f0 <- function(n,
+                      alpha, 
+                      beta,
+                      tau,
+                      data_EC, # available EC data
+                      r, # the ratio of variance in EC and variance in control of RCT
+                      d_X, # the ratio of f(X|R=1) with f(X|R=0)
+                      pi, # given propensity score
+                      gamma1 # the ratio of two kappa
+                      ){
+    # n is the sample size for the treatment group
+    # pi is the proensity score for the treatment group
+    return(n)
+    }
+  
+  eval_g1 <- function(n, 
+                      alpha, 
+                      beta,
+                      tau,
+                      data_EC, # available EC data
+                      r, # the ratio of variance in EC and variance in control of RCT
+                      d_X, # the ratio of f(X|R=1) with f(X|R=0)
+                      pi, # given propensity score
+                      gamma1 # the ratio of two kappa
+                      ){
+    # Step 0: given propensity score function - skip, let's use 1/2
+    
+    # Step 1: estimate V(Y|X,R=0) using data_EC
+    # var_X_EC <- var_fun(data_EC)
+    var_X_EC <- 1
+    
+    # Step 2: estimate V(Y|X,R=1,A=0)
+    var_X_RCT_0 <- r * var_X_EC
+    
+    # Step 3: estimate kappa 0 and kappa 1
+    kappa_0 <- mean(var_X_RCT_0)
+    kappa_1 <- gamma1 * kappa_0
+    
+    # Step 4: estimate V(Y|R=1,A=1), V(Y|R=1, A=0) 
+    #  Step 4 relies on the d(X) function 
+    var_EC <- var(data_EC$Y_EC)
+    var_RCT_0 <- var_EC
+    var_RCT_1 <- var_EC
+    
+    # Step 5: calculate d(X) - skip, let's use the true value 
+    
+    # Step 6: plug in to get nu^2
+    N_EC <- nrow(data_EC)
+    N_RCT <- n - N_EC
+    r_R <- N_RCT / N_EC
+    term1 <- 1 / N_RCT / pi * mean(var_X_RCT_0)
+    term2 <- 1 / N_RCT * (var_EC -  mean(var_X_RCT_0))
+    term3 <- 1 / N_RCT * mean(r * (1 - pi)/(1 - pi + r / d_X / r_R)^2 * var_X_EC)
+    term4 <- 1 / N_RCT * (var_EC - mean(var_X_RCT_0)) 
+    term5 <- 1 / N_RCT * mean(r^2/r_R/(1 - pi + r / d_X / r_R)^2 * var_X_EC)
+    nu2 <- n * (term1 + term2 + term3 + term4 + term5)
+    sqrt_nu2 <- sqrt(nu2)
+    
+    res <- 1 - beta -
+      pnorm(qnorm(alpha/2) + sqrt(n) * tau / sqrt_nu2) -
+      pnorm(qnorm(alpha/2) - sqrt(n) * tau / sqrt_nu2)
+    return(res)
+  }
 
-# Generate RCT data based on given sample size
-f_gen_sim <- function(
-    seed = 123,
-    NE = 1000, # sample size of EC data
-    Nt = , # sample size of treatment group in RCT data
-    muX = 1, 
-    sdX = 1, 
-    pi = 0.5, # propensity score in RCT 
-    ){
-  # sed random seed for each simulation
-  set.seed(seed)
   
-  # generate the pre-treatment covariates
-  X <- rnorm(n, mean = muX, sd = sdX)
-  
-  # generate A by using pi
-  A <- sample(c(1,0), size = n, prob = c(pi, 1-pi))
+  # Use the nonlinear optim function to find the smallest sample size
+  # need to figure out the estimated nu
+  res2 <- nloptr(x0 = c(1200),
+                 eval_f = eval_f0,
+                 lb = c(1010),
+                 ub = c(Inf),
+                 eval_g_ineq = eval_g1,
+                 opts = list("algorithm"="NLOPT_LN_COBYLA",
+                             "xtol_rel"=1.0e-8),
+                 data_EC = data_EC,
+                 r = 1,
+                 d_X = 1,
+                 pi = 0.8, # pi_A
+                 gamma1 = 1,
+                 alpha = 0.05,
+                 beta = 0.2,
+                 tau = 0.8)
+  print( res2 )
   
 }
 
 
-
-
-
-#################################################
+###############################################
+####### Nonlinear optimization function #######
 ####### Default RCT Sample Size - Schuler #######
 # Need to estimate nu for IF based simple case
 {
-  
+  ### The following optimization for the traditional RCT ###
   # write the optimization function using Schuler's example
   eval_f0 <- function(n,
                       alpha, 
                       beta,
                       tau,
-                      nu){
+                      sigma){
     # n is the sample size for the treatment group
     # pi is the proensity score for the treatment group
     return(n)
@@ -124,14 +207,15 @@ f_gen_sim <- function(
   
   # the default two sample test case
   # In the form g(n) <= 0
+  # Check Schuler's paper for page 2
   eval_g0 <- function(n, 
                       alpha, 
                       beta,
                       tau,
-                      nu){
+                      sigma){
     res <- 1 - beta -
-      pnorm(qnorm(alpha/2) + sqrt(n) * tau / 2 / nu) -
-      pnorm(qnorm(alpha/2) - sqrt(n) * tau / 2 / nu)
+      pnorm(qnorm(alpha/2) + sqrt(n) * tau / 2 / sigma) -
+      pnorm(qnorm(alpha/2) - sqrt(n) * tau / 2 / sigma)
     return(res)
   }
   
@@ -146,38 +230,8 @@ f_gen_sim <- function(
                  alpha = 0.05,
                  beta = 0.2,
                  tau = 0.8,
-                 nu = sqrt(5))
+                 sigma = sqrt(5))
   print( res1 )
-  
-  
-  # the IF based test case
-  # In the form g(n) <= 0
-  eval_g1 <- function(n, 
-                      alpha, 
-                      beta,
-                      tau,
-                      nu){
-    res <- 1 - beta -
-      pnorm(qnorm(alpha/2) + sqrt(n) * tau / nu) -
-      pnorm(qnorm(alpha/2) - sqrt(n) * tau / nu)
-    return(res)
-  }
-  
-  # Use the nonlinear optim function to find the smallest sample size
-  # need to figure out the estimated nu
-  res2 <- nloptr(x0=c(100),
-                 eval_f = eval_f0,
-                 lb = c(0),
-                 ub = c(Inf),
-                 eval_g_ineq = eval_g1,
-                 opts = list("algorithm"="NLOPT_LN_COBYLA",
-                             "xtol_rel"=1.0e-8),
-                 alpha = 0.05,
-                 beta = 0.2,
-                 tau = 0.8,
-                 nu = )
-  print( res2 )
-  
   
   # this power function is matched with the following case 
   power_f <- function(n,
@@ -199,6 +253,31 @@ f_gen_sim <- function(
   
   
 }
+
+
+
+
+############################################################
+####### Generate RCT data based on given sample size #######
+f_gen_sim <- function(
+    seed = 123,
+    NE = 1000, # sample size of EC data
+    Nt = , # sample size of treatment group in RCT data
+    muX = 1, 
+    sdX = 1, 
+    pi = 0.5 # propensity score in RCT 
+){
+  # sed random seed for each simulation
+  set.seed(seed)
+  
+  # generate the pre-treatment covariates
+  X <- rnorm(n, mean = muX, sd = sdX)
+  
+  # generate A by using pi
+  A <- sample(c(1,0), size = n, prob = c(pi, 1-pi))
+  
+}
+
 
 
 ################################################
